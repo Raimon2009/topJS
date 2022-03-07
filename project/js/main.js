0ce0ef5f635242f4a9194ce5c119423b3a2d4e53
Alexey <av_davidoff@mail.ru>
2022-03-07 21:53:56 +0300

    ДЗ6

--- project/js/main.js
+++ project/js/main.js
@@ -1,72 +1,21 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 const app = new Vue({
-  el: '#app',
-  data: {
-    catalogUrl: '/catalogData.json',
-    cartItem: [],
-    products: [],
-    imgCatalog: 'https://via.placeholder.com/200x150',
-    imgCart: 'https://via.placeholder.com/50x100',
-    searchLine: '',
-    isVisibleCart: 'false',
-    filters: [],
-  },
-  methods: {
-    getJson(url) {
-      return fetch(url)
-        .then(result => result.json())
-        .catch(error => {
-          console.log(error);
-        })
+    el: '#app',
+    data: {
+        userSearch: '',
     },
-    addProduct(product) {
-      this.getJson(`${API}/addToBasket.json`)
-        .then(data => {
-          if (data.result === 1) {
-            let find = this.cartItem.find(el => el.id_product === product.id_product);
-            if (find) {
-              find.quantity++;
-            } else {
-              let prod = Object.assign({ quantity: 1 }, product);
-              this.cartItem.push(prod)
-            }
-          } else {
-            alert('Error');
-          }
-        })
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
     },
-    remove(item) {
-      this.getJson(`${API}/deleteFromBasket.json`)
-        .then(data => {
-          if (data.result === 1) {
-            if (item.quantity > 1) {
-              item.quantity--;
-            } else {
-              this.cartItem.splice(this.cartItem.indexOf(item), 1)
-            }
-          }
-        })
-    },
-    filterGoods() {
-      let regExp = new RegExp(this.searchLine, 'i');
-      this.filters = this.products.filter(el => regExp.test(el.product_name))
-    },
-  },
-  // beforeCreated() { },
-  created() {
-    this.getJson(`${API}/getBasket.json`)
-      .then(data => {
-        for (let el of data.contents) {
-          this.cartItem.push(el);
-        }
-      });
-    this.getJson(`${API + this.catalogUrl}`)
-      .then(data => {
-        for (let el of data) {
-          this.products.push(el);
-          this.filters.push(el);
-        }
-      });
-  },
+    mounted() {
+        console.log(this);
+    }
 });
+
